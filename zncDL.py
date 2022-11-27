5337139a90bc72732981567892c2530b5f03e943
Edoardo Balducci <81587335+Bbalduzz@users.noreply.github.com>
2022-11-27 15:42:06 +0100

    Update zncDL.py

--- zncDL.py
+++ zncDL.py
@@ -1,8 +1,7 @@
-import requests, os
+import requests, os, shutil
 from bs4 import BeautifulSoup
-from svglib.svglib import svg2rlg
-from reportlab.graphics import renderPDF
 from natsort import natsorted
+import cairosvg
 
 try:
     os.mkdir('pages')
@@ -12,16 +11,20 @@ except:
 
 with open('cookies.txt', 'r') as f: cookie = f.readline()
 COOKIE = {'Cookie': cookie}
-BASE_URL = input('Enter book URL (search toc.xml): \n')
+BASE_URL = input('[+] Enter book URL (search toc.xml): \n')
 
 def progress_bar(progress, total):
     percent = 100 * (progress / float(total))
     bar = '█' * int(percent) + '-' * (100 - int(percent))
-    print(f'\r Downloading book... |{bar}| {percent:.2f}%', end='\r')
+    print(f'\r[+] Downloading book... |{bar}| {percent:.2f}%', end='\r')
 
-toc_req = requests.get(f'{BASE_URL}/toc.xml', headers=COOKIE)
+toc_req = requests.get(f'{BASE_URL}/content.opf', headers=COOKIE)
 soup = BeautifulSoup(toc_req.content, 'xml')
-npages = int(soup.find('toc').find_all('node')[-1]['href'].removesuffix('.xhtml').removeprefix('page'))
+npages = int(soup.select('itemref')[-1]['idref'].removeprefix('page'))
+book_title = soup.find('dc:title').text
+book_desc = soup.find('dc:description').text
+book_author = soup.find('dc:author').text
+book_isbn = soup.find('dc:identifier').text.split(':')[2]
 
 def download_and_create():
     progress_bar(0, npages)
@@ -29,9 +32,8 @@ def download_and_create():
         page_data = requests.get(f'{BASE_URL}/images/page{n:04d}.svgz', cookies=COOKIE).content
         with open(f'pages/page{n:04d}.svg', 'wb') as f:
             f.write(page_data)
+        cairosvg.svg2pdf(url=f'pages/page{n:04d}.svg', write_to=f'pdfs/page{n:04d}.pdf')
         progress_bar(n+1, npages)
-        drawing = svg2rlg(f"pages/page{n:04d}.svg")
-        renderPDF.drawToFile(drawing, f"pdfs/page{n:04d}.pdf")
 
 def merge_pdfs():
     from PyPDF2 import PdfMerger
@@ -40,7 +42,20 @@ def merge_pdfs():
     pdfs = natsorted(pdfs)
     for pdf in pdfs:
         merger.append(open(f'pdfs/{pdf}', 'rb'))
-    with open('book.pdf', 'wb') as book_pdf:
+    with open(f'{book_title}.pdf', 'wb') as book_pdf:
         merger.write(book_pdf)
+
+print(f'''
+[+] Book Found:
+    - title: {book_title}
+    - description: {book_desc}
+    - author: {book_author}
+    - isbn: {book_isbn}
+    - pages: {npages}
+''')
+
 download_and_create()
 merge_pdfs()
+shutil.rmtree('pages')
+shutil.rmtree('pdfs')
+print('\nDone :)')
